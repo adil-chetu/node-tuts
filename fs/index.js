4bab3819d37751db67c153169f3afd9920d94c89
Adil Choudhary <adil.chetu@yahoo.com>
2022-07-19 13:19:23 +0530

    deleted fs file

--- fs/index.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-
-fs.writeFileSync('read.txt','This is my text file i am learning node js here.')
-
-fs.appendFileSync('read.txt', 'I am adding the extra text here.')
-
-const data = fs.readFileSync('bio.txt', 'utf-8')
-console.log(data)
