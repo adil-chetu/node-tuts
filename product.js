4e71805e5773742a34fc5abad68e8fb6d35b26a2
Adil Choudhary <adil.chetu@yahoo.com>
2022-07-25 08:07:18 +0530

    delete all files

--- product.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose')
-/*product schema that is used to validate the field name */
-const productSchema = new mongoose.Schema({
-    name:String,
-    price:Number,
-    brand:String,
-    category:String
-})
-module.exports = mongoose.model('products', productSchema)
